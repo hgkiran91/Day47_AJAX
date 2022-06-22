15a29e0d309f65f6de7e51ff9ef8de6991fa2b16
kiran@DESKTOP-P65JLIU <kiranmcehsn@gmail.com>
2022-06-22 18:30:23 +0530

    Added: UC3 To view Employee data using AJAX and Promise

--- AjaxCallback.js
+++ AjaxCallback.js
@@ -5,47 +5,53 @@ function showTime(){
     return date.getHours()+ "hrs: "+date.getMinutes()+"mins: "+date.getSeconds()+"secs"; 
 }
 
-function makeAJAXCall(methodType, url, callback, async=true, data=null){
-    let xhr=new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-        // console.log(methodType + " State Changed called at: " + showTime() + " Ready State: " +
-        //             xhr.readyState + " Status: " + xhr.status);
-        if(xhr.readyState===4){
-            if(xhr.status===200 || xhr.status===201)
-            callback(xhr.responseText);
-        } else if(xhr.status >= 400){
-            console.log("Handle 400 Client Error or 500 Server Error at: " + showTime());
+function makPromiseCall(methodType, url, async=true, data=null){
+    return new Promise(function (resolve, reject) {
+        let xhr=new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            // console.log(methodType + " State Changed called at: " + showTime() + " Ready State: " +
+            //             xhr.readyState + " Status: " + xhr.status);
+            if(xhr.status.toString().match('^[2][0-9]{2}$')){
+                resolve(xhr.responseText);
+            } else if(xhr.status.toString().match('^[4,5][0-9]{2}$')){
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("XHR Failed");
+            }
         }
-    }
-    xhr.open(methodType, url, async);
-    // xhr.send();
-    if(data){
-        console.log(JSON.stringify(data));
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify(data));
-    }else {
-        xhr.send();
-    }
-    console.log(methodType +" request sent to the server at: "+showTime());
+        xhr.open(methodType, url, async);
+        // xhr.send();
+        if(data){
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        }else {
+            xhr.send();
+        }
+        console.log(methodType +" request sent to the server at: "+showTime());
+    });
 }
 
 const getURL="http://localhost:3000/employees/5";
-
-function getUserDetails(data){
-    console.log("Get User Data at: "+ showTime() + "data: "+ data);
-}
-makeAJAXCall("Get", getURL, getUserDetails, true);
-console.log("Made GET AJAX Call to server at: "+showTime());
+makPromiseCall("GET", getURL, true)
+    .then(responseText => {
+    console.log("Get User Data at: "+ showTime() + "data: "+ responseText);
+    })
+    .catch(error => console.log("Get Error Status: " + JSON.stringify(error)));
 
 const deleteURL="http://localhost:49856/employees/4";
-function userDeleted(data){
-    console.log("User deleted: "+data);
-}
-makeAJAXCall("Delete", deleteURL,userDeleted,false);
+makPromiseCall("DELETE", deleteURL, false)
+    .then(responseText => {
+    console.log("User Deleted Data at: "+ showTime() + "data: "+ responseText);
+    })
+    .catch(error => console.log("Delete Error Status: " + JSON.stringify(error)));
 
 const postURL="http://localhost:3000/employees";
 const emplData={"name": "Harry", "salary": "5000"};
-function userAdded(data){
-    console.log("User Added: "+data);
-}
-makeAJAXCall("Post",postURL,userAdded,true,emplData)
+makPromiseCall("POST", postURL, true, emplData)
+    .then(responseText => {
+    console.log("User Added Data at: "+ showTime() + "data: "+ responseText);
+    })
+    .catch(error => console.log("Post Error Status: " + JSON.stringify(error)));
